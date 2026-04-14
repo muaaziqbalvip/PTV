78ae4c8b020147b02c3e2017b3565112afc6e0c7
muaaziqbalvip <muaaziqbal.vip@gmail.com>
2026-04-14 18:40:59 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+BOT_TOKEN = "8700778314:AAECDc3KN8BzDD_-4_Clkv0zGxhgC1WRw5g"
 ADMIN_PASSWORD = "123456"
 
-FIREBASE_DB = "https://YOUR_PROJECT-default-rtdb.firebaseio.com/"
-LOGO_URL = "https://i.ibb.co/your-logo.png"
+FIREBASE_DB = "https://ramadan-2385b-default-rtdb.firebaseio.com"
+LOGO_URL = "https://i.ibb.co/d0xwVb2b/file-00000000246471fa9014d17924d4447a.png"
